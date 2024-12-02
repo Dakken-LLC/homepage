a8404de50311b6d40c2844ada1622053784fcd2c
Iida <2002kousei@gmail.com>
2024-12-02 17:42:22 +0900

    test

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <h3>DAkken LLC.</h3>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">home</Link>
+                </li>
+                <li>
+                    <Link to="/business">Business</Link>
+                </li>
+                <li><a href="member">Member</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
+
