2c236a3b7f50c41eba310d4d1559ab2886e58146
Iida <2002kousei@gmail.com>
2024-12-21 04:45:06 +0900

    UIコンポーネントの追加とスタイルの調整、依存関係の更新

--- src/pages/Business.tsx
+++ src/pages/Business.tsx
@@ -2,8 +2,29 @@ import "../App.css";
 
 export default function BusinessPage() {
   return (
-    <div className="page_title">
-      <h1>事業内容</h1>
+    <div className="container text-center">
+      <img src={dakken_home} className="page_top_image" alt="Top Image" />
+      <div className="page_title">
+        <h1>事業内容</h1>
+        <div className="page_content">
+          <h3>
+            情報科学の知見を学生の
+            <span className="blue-text">若い発想力と実行力で</span>
+            <br></br>
+            デジタル時代の企業の発展に活かす。
+          </h3>
+          <br></br>
+          <br></br>
+          <br></br>
+          <h4>
+            大学で学んだデータサイエンスを中心とした情報科学の知見を<br></br>
+            学生である私たちの柔軟な発想力を活かし、企業のデジタル化に貢献します。
+            <br></br>
+            現代社会において大量に存在する様々なデータを適切に対処して<br></br>
+            最適化された学生による面白いアイデアを提供します。
+          </h4>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,11 @@
 import dakken_logo from "../assets/dakken_llc_logo.png";
+import dakken_home from "../assets/dakken_home_1221.png"
 import "../App.css";
 
 export default function HomePage() {
   return (
-    <div className="container text-center">
-      <h1>Coming Soon …</h1>
-      <img src={dakken_logo} className="dakken_logo" />
-      <section className="page-section" id="services">
-        <div className="service">
-          <div className="center-text">Welcome to DAkken LLC!</div>
-        </div>
-      </section>
+    <div className="container">
+      <img src={dakken_home} className="page_top_image" alt="Top Image" />
     </div>
   );
 }
